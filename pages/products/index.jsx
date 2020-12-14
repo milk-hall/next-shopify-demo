e84302f50c039f920040518132ae5bb65ec5b7d1
wucaixin <297912123@qq.com>
2020-12-14 18:05:28 +0800

    :new: 配置 graphql请求地址

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,6 +1,10 @@
+import { useApolloClient } from "@apollo/client"
 import Layout from "../../components/Layout"
+import { initializeApollo } from "../../lib/apollo"
 
 const Products = ()=>{
+  const apolloClient = initializeApollo()
+
   return(
     <Layout title='Products'>
       Products
